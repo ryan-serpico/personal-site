d142dca4f960e86e28f9c91016066ea069120e23
Ryan Serpico <ryguy360@gmail.com>
2025-09-03 23:15:47 -0500

    Remove base path for root domain deployment

--- astro.config.mjs
+++ astro.config.mjs
@@ -6,7 +6,6 @@ import { defineConfig } from 'astro/config';
 // https://astro.build/config
 export default defineConfig({
     site: 'https://ryan-serpico.github.io',
-    base: '/personal-site',
     vite: {
         plugins: [tailwindcss()]
     },
